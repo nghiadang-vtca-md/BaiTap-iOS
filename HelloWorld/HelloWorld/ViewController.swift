d6e7e63e0dc90906d438f94996475ec9e2fbc9cc
iMac_VTCA <imac_vtca@iMac-VTCAs-iMac-16.local>
2019-10-16 20:02:10 +0700

    add Project HelloWorld

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by iMac_VTCA on 10/16/19.
+//  Copyright © 2019 iMac_VTCA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
