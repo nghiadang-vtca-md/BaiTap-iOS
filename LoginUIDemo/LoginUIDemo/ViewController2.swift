570010f631ad4c9d1cb87da41036ff180c01a73e
iMac_VTCA <imac_vtca@iMac-VTCAs-iMac-16.local>
2019-10-18 21:18:07 +0700

    accomplished baitap random

--- LoginUIDemo/LoginUIDemo/ViewController2.swift
+++ LoginUIDemo/LoginUIDemo/ViewController2.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController2: UIViewController {
-
+    // Properties
+    
+    @IBOutlet weak var btnRandom: UIButton!
+    
+    // Actions
+    @IBAction func btnRandom_TUI(_ sender: UIButton) {
+        self.i += 1
+        createLabel(title: self.i)
+    }
+    var i = 0
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    func createLabel(title: Int) -> Void {
+        let randomInt_x = Int.random(in: 1...384)
+        let randomInt_y = Int.random(in: 1...866)
+        
+        let newLabel = UILabel(frame: CGRect(x: randomInt_x, y: randomInt_y, width: 30, height: 30))
+        newLabel.text = String(title)
+        newLabel.backgroundColor = .yellow
+        
+        self.view.addSubview(newLabel)
+    }
+    
+    
     
 
     /*
