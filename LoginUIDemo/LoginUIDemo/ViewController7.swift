93434e1276a3b77fc7f19bcead528f421ce582e4
iMac_VTCA <imac_vtca@iMac-VTCAs-iMac-16.local>
2019-10-23 19:08:23 +0700

    Exercise Calculator v2.0

--- LoginUIDemo/LoginUIDemo/ViewController7.swift
+++ LoginUIDemo/LoginUIDemo/ViewController7.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController7: UIViewController {
+class ViewController7: UIViewController, UITextFieldDelegate {
     // Properties
     @IBOutlet weak var tfX: UITextField!
     @IBOutlet weak var tfY: UITextField!
@@ -18,30 +18,41 @@ class ViewController7: UIViewController {
     @IBAction func btnSubmit_tui(_ sender: UIButton) {
         let alertController = UIAlertController(title: "Options", message: "Choose the operation", preferredStyle: .actionSheet)
         
+//        let valueX_String = tfX.text ?? "0"
+//        let valueY_String = tfY.text ?? "0"
+//
+//        let valueX: Int = Int(valueX_String) ?? 0
+//        let valueY: Int = Int(valueY_String) ?? 0
         
+        
+        guard let x = tfX.text, !x.isEmpty else {
+            showAlertEmptyField(field: "x")
+            return
+        }
+        guard let y = tfY.text, !y.isEmpty else {
+            showAlertEmptyField(field: "y")
+            return
+        }
+        
+        let valueX = Int(x) ?? 0
+        let valueY = Int(y) ?? 0
         
         let actionCancel = UIAlertAction(title: "Cancel", style: .cancel) { (_) in
             print("action cancel")
         }
         
         let actionAdd = UIAlertAction(title: "Add (+)", style: .default) { (_) in
-            print("+")
-            let alertStyle = UIAlertController(title: "Result", message: "a + b = ab", preferredStyle: .alert)
-            let actionOk = UIAlertAction(title: "Ok", style: .default) { (_) in
-                print("action Ok")
-            }
-            alertStyle.addAction(actionOk)
-            self.present(alertStyle, animated: true, completion: nil)
+            self.calculateAndShowResultAlert(x: valueX, y: valueY, operate: "+")
             
         }
         let actionMinus = UIAlertAction(title: "Minus (-)", style: .default) { (_) in
-            print("-")
+            self.calculateAndShowResultAlert(x: valueX, y: valueY, operate: "-")
         }
         let actionMultiply = UIAlertAction(title: "Multiply (*)", style: .default) { (_) in
-            print("*")
+            self.calculateAndShowResultAlert(x: valueX, y: valueY, operate: "*")
         }
         let actionDivide = UIAlertAction(title: "Divide (/)", style: .default) { (_) in
-            print("/")
+            self.calculateAndShowResultAlert(x: valueX, y: valueY, operate: "/")
         }
         
         let actionReEnter = UIAlertAction(title: "Re-enter", style: .destructive) { (_) in
@@ -58,6 +69,15 @@ class ViewController7: UIViewController {
         self.present(alertController, animated: true, completion: nil)
         
         
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            if textField == tfX {
+                tfY.becomeFirstResponder()
+            } else if textField == tfY {
+                self.view.endEditing(true)
+            }
+            return true
+        }
+        
     }
     
     
@@ -66,6 +86,38 @@ class ViewController7: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func showAlertEmptyField(field: String){
+        let alertStyle = UIAlertController(title: "Error", message: "\(field) is empty!", preferredStyle: .alert)
+        let actionOk = UIAlertAction(title: "OK", style: .default) { (_) in
+            print("action OK")
+        }
+        alertStyle.addAction(actionOk)
+        self.present(alertStyle, animated: true, completion: nil)
+    }
+    
+    func calculateAndShowResultAlert(x: Int, y: Int, operate: String){
+        var result: String
+        switch operate {
+        case "+":
+            result = String(x + y)
+        case "-":
+            result = String(x - y)
+        case "*":
+            result = String(x * y)
+        case "/":
+            result = x == 0 || y == 0 ? "0" : String(x / y)
+        default:
+            return
+        }
+        
+        let alertStyle = UIAlertController(title: "Result", message: "\(x) \(operate) \(y) = \(result)", preferredStyle: .alert)
+        let actionOk = UIAlertAction(title: "Ok", style: .default) { (_) in
+            print("action Ok")
+        }
+        alertStyle.addAction(actionOk)
+        self.present(alertStyle, animated: true, completion: nil)
     }
     
 
