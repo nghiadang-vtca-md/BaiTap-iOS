a8ada2c79176050986159d67c1398c162a13ffb8
quynh_gamedev <quynh.fullstackdev@gmail.com>
2019-11-25 21:22:09 +0700

    Demo StackView

---
+++ DemoStackView/DemoStackView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoStackView
+//
+//  Created by iMac_VTCA on 11/25/19.
+//  Copyright © 2019 iMac_VTCA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DemoStackView/DemoStackView/ViewController1.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController1.swift
+//  DemoStackView
+//
+//  Created by iMac_VTCA on 11/25/19.
+//  Copyright © 2019 iMac_VTCA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
