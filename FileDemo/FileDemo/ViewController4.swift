d8114e2dde9af7fcdb17ed018b89f99349c5ff43
user159518 <user159518@AS121.local>
2019-12-18 17:27:59 +0800

    BT MP3 Player v1.0

--- FileDemo/FileDemo/ViewController4.swift
+++ FileDemo/FileDemo/ViewController4.swift
@@ -7,20 +7,113 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController4: UIViewController {
+    
+    var player: AVAudioPlayer!
+    
+    @IBOutlet weak var lblStartTime: UILabel!
+    @IBOutlet weak var lblEndTime: UILabel!
+    @IBOutlet weak var sliderDuration: UISlider!
+    @IBOutlet weak var btnPlay: UIButton!
+    
+    var timer: Timer?
+    var timeDuration = 0
+    var currentTime = 0
+    var isLoop = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        guard let path = Bundle.main.path(forResource: "If_I_Had_a_Chicken", ofType: "mp3") else {
+            return
+        }
+        print("path bundle: \(path)")
+        guard let url = URL(string: path) else {
+            return
+        }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        lblEndTime.text = convertTimeIntervalToString(time: player.duration)
+        timeDuration = Int(round(player.duration))
+        
+    }
+    
+    @IBAction func btnLoop_Click(_ sender: UIButton) {
+        isLoop = !isLoop
+        if isLoop {
+            player.stop()
+            player.numberOfLoops = -1
+            player.play()
+            sender.alpha = CGFloat(1.0)
+        } else {
+            player.stop()
+            player.numberOfLoops = 0
+            player.play()
+            sender.alpha = CGFloat(0.3)
+        }
+    }
+    
+    @IBAction func slider_ValueChange(_ sender: UISlider) {
+        currentTime = Int(sender.value * Float(player.duration))
+        player.stop()
+        player.currentTime = TimeInterval(currentTime)
+        player.play()
     }
     
     @IBAction func play(_ sender: UIButton) {
-        if let image = UIImage(named: "pause-icon.png") {
-            sender.setImage(image, for: .normal)
+        // tag = 1 -> play ; tag = 2 -> pause
+        if sender.tag == 1 {
+            if let image = UIImage(named: "pause-icon.png") {
+                sender.setImage(image, for: .normal)
+                sender.tag = 2
+                player.play()
+                timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(onTimerFires), userInfo: nil, repeats: true)
+            }
+        } else {
+            if let image = UIImage(named: "play-icon.png") {
+                sender.setImage(image, for: .normal)
+                sender.tag = 1
+                timer?.invalidate()
+                player.pause()
+            }
         }
-       
+    }
+    
+    @objc func onTimerFires()
+    {
+        
+        currentTime = Int(round(player.currentTime))
+        print(currentTime)
+        lblStartTime.text = convertTimeIntervalToString(time: player.currentTime)
+        let a: Float = Float(player.currentTime / player.duration)
+        sliderDuration.setValue(a, animated: true)
+        print(a)
+        if currentTime == self.timeDuration && player.numberOfLoops == 0 {
+            self.timer?.invalidate()
+            self.timer = nil
+            if let image = UIImage(named: "play-icon.png") {
+                btnPlay.setImage(image, for: .normal)
+                btnPlay.tag = 1
+                player.pause()
+            }
+        }
+    }
+    
+    func updateTimeLabel() {
+        //let a = player.duration
+    }
+    
+    func convertTimeIntervalToString(time: TimeInterval) -> String {
+        let minutes = floor(time / 60)
+        let seconds = round(time - minutes * 60)
+        return "\(Int(minutes)):\(Int(seconds))"
     }
     
     /*
