5691ab153b45df8a53d97ccbb99bbab1551a49b0
DUC-NGHIA <ducnghia1205@gmail.com>
2020-04-24 18:55:08 +0700

    [Feature: Show users from FireStore]:   - loaduser by filter segment control   - Change 'Call' tab item to 'Chats'   - create new chat present to loaduser

---
+++ BaiTapPractice/WhatsAppClone/WhatsAppClone/ChatsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ChatsViewController.swift
+//  WhatsAppClone
+//
+//  Created by nghiadang1205 on 4/24/20.
+//  Copyright © 2020 NghiaDang. All rights reserved.
+//
+
+import UIKit
+
+class ChatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: IBActions
+    @IBAction func createNewChatButtonPressed(_ sender: Any) {
+        let usersVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "usersTableView") as! UsersTableViewController
+        self.navigationController?.pushViewController(usersVC, animated: true)
+    }
+    
+}
---
+++ BaiTapPractice/WhatsAppClone/WhatsAppClone/UsersTableViewController.swift
@@ -0,0 +1,125 @@
+//
+//  UsersTableViewController.swift
+//  WhatsAppClone
+//
+//  Created by nghiadang1205 on 4/24/20.
+//  Copyright © 2020 NghiaDang. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import ProgressHUD
+
+class UsersTableViewController: UITableViewController, UISearchResultsUpdating {
+    
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var filterSegmentedControl: UISegmentedControl!
+    
+    var allUsers: [FUser] = []
+    var filteredUsers: [FUser] = []
+    var allUsersGroupped = NSDictionary() as! [String: [FUser]]
+    var sectionTitleList: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadUsers(filter: kCITY)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allUsers.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? UserTableViewCell else { return UITableViewCell() }
+        
+        cell.generateCellWith(fUser: allUsers[indexPath.row], indexPath: indexPath)
+        
+        return cell
+    }
+    
+    //
+    
+    func loadUsers(filter: String) {
+        ProgressHUD.show()
+        
+        var query: Query!
+        
+        switch filter {
+        case kCITY:
+            query = reference(.User).whereField(kCITY, isEqualTo: FUser.currentUser()!.city).order(by: kFIRSTNAME, descending: false)
+        case kCOUNTRY:
+            query = reference(.User).whereField(kCOUNTRY, isEqualTo: FUser.currentUser()!.city).order(by: kFIRSTNAME, descending: false)
+        default:
+            query = reference(.User).order(by: kFIRSTNAME, descending: false)
+        }
+        
+        query.getDocuments { (snapshot, error) in
+            self.allUsers = []
+            self.sectionTitleList = []
+            self.allUsersGroupped = [:]
+            
+            if error != nil {
+                print(error!.localizedDescription)
+                ProgressHUD.dismiss()
+                self.tableView.reloadData()
+                return
+            }
+            
+            guard let snapshot = snapshot else {
+                ProgressHUD.dismiss()
+                return
+            }
+            
+            if !snapshot.isEmpty {
+                for userDictionary in snapshot.documents {
+                    let userDic = userDictionary.data() as NSDictionary
+                    let fUser = FUser(_dictionary: userDic)
+                    
+                    if fUser.objectId != FUser.currentId() {
+                        self.allUsers.append(fUser)
+                    }
+                }
+                
+                // split to groups
+            }
+            
+            self.tableView.reloadData()
+            ProgressHUD.dismiss()
+        }
+    }
+    
+    // MARK: IBActions
+    @IBAction func filterSegmentValueChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            loadUsers(filter: kCITY)
+        case 1:
+            loadUsers(filter: kCOUNTRY)
+        case 2:
+            loadUsers(filter: "")
+        default:
+            return
+        }
+    }
+
+    // MARK: Search controller functions
+    func filterContentForSearchText(searchText: String, scope: String = "All") {
+        filteredUsers = allUsers.filter({ (user) -> Bool in
+            return user.firstname.lowercased().contains(searchText.lowercased())
+        })
+        
+        tableView.reloadData()
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchText: searchController.searchBar.text!)
+    }
+
+}
